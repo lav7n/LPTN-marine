010ecdf5a29c9960d00449025a65c6cf0e5a8593
Ishaan Gakhar <106798053+IshaanGakhar@users.noreply.github.com>
2024-08-21 21:54:20 +0530

    Update train_utils.py

--- src/utils/train_utils.py
+++ src/utils/train_utils.py
@@ -119,6 +119,7 @@ class Epoch:
 
                 # update metrics logs
                 for metric_fn in self.metrics:
+                    print('Computing :', metric_fn.__name__)
                     y_pred = torch.argmax(y_pred, dim=1)
                     metric_value = metric_fn(y_pred, y.int()).cpu().detach().numpy()
                     #metric_value = metric_fn(y_pred, y.int()).detach().cuda()
